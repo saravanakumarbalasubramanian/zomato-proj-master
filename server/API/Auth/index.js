4f34bbb6dde830257164bfd8b0dbad2723de01b1
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-08 03:42:10 +0530

    add static and method function

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 // Models
 // we are destructuring the UserModel because we are simply exported the usermodel we are not exported by default so we have to destructure it 
-import {UserModel} from "../../database/user";
+import { UserModel } from "../../database/user";
 
 const Router = express.Router();
 
@@ -16,36 +16,19 @@ Access   Public
 Method   POST
 */
 
-Router.post("/signup", async(req, res) =>{
-   try {
-       const {email, password, fullname, phoneNumber} = req.body.credentials;
+Router.post("/signup", async (req, res) => {
+  try {
 
-       // check whether email exist
+    await UserModel.findByEmailAndPhone(req.body.credentials);
 
-       const checkUserByEmail = await UserModel.findOne({email});
-       const checkUserByPhone = await UserModel.findOne({phoneNumber});
-
-       if( checkUserByEmail || checkUserByPhone ) {
-        return res.json({ error: "User already exist!"});
-       }
-       // hash password
-       const bcryptSalt = await bcrypt.genSalt(5);
-
-       const hashedPassword = await bcrypt.hash(password, bcryptSalt);
-
-       // Save to DB
-       await UserModel.create({
-         ...req.body.credentials,
-         password: hashedPassword,
-       });
-       // generate JWT auth Token
-        const token = jwt.sign({user: { fullname, email}} , "ZomatoAPP");
-       // Return
-       return res.status(200).json({ token, status:"success"});
-
-   } catch (error) {
-     return res.status(500).json({error: error.meaasge});
-   }
+    const newUser = await UserModel.create(req.body.credentials);
+    // generate JWT auth Token
+    const token = newUser.generateJwtToken();
+    // Return
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 export default Router;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+// IMporting env Variables
+require("dotenv").config();
+
+//Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+// Import MicroServices Routes 
+import Auth from "./API/Auth"
+
+// DataBase Connection 
+import ConnectDB from "./database/connection";
 
 
 const zomato = express();
@@ -11,6 +21,11 @@ zomato.use(express.urlencoded({extended:false}));
 zomato.use(helmet());
 zomato.use(cors());
 
+// Application Routes
+zomato.use("/auth", Auth);
+
 zomato.get("/" , (req, res) => res.json({message: "Setup Success"}));
 
-zomato.listen(5, () => console.log("Server is Running 🚀")); 
+zomato.listen(300, () => ConnectDB().then(() => console.log("Server is Running 🚀"))
+  .catch(() => console.log("server is running, but the database connection failed "))
+); 

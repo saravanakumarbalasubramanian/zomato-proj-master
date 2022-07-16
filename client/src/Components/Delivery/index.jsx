8d52be2d110822835fe4909f3a01453084958ae7
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-16 18:39:18 +0530

    restaurant card'

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //components
 import DeliveryCarousal from './DeliveryCarousal';
@@ -6,11 +6,58 @@ import Brand from './Brand';
 import RestaurantCard from '../RestaurantCard';
 
 const Delivery = () => {
+
+       const [ restaurantList, setRestaurantList] = useState ([
+         {
+           _id: "12345",
+           photos: [
+            "https://b.zmtcdn.com/data/pictures/chains/8/18443088/d26f90bcacb3caeb175479a90e1eddb2.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+           ],
+           name: "Samosa Party",
+           cuisine: ["Street Food", "Beverages", "Tea"],
+           averageCost: 100,
+           isPro: true,
+           isOff: "80",
+           durationOfDelivery: 47,
+           restaurantReviewValue: 5,
+         },
+         {
+           _id: "12345-1",
+           photos: [
+            "https://b.zmtcdn.com/data/pictures/chains/6/50036/db2271e47065db0a611237066279297b_featured_v2.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+           ],
+           name: "AL-Bek",
+           cuisine: ["Arabin", "North Indian", "Chinese"],
+           averageCost: 150,
+           isPro: false,
+           isOff: false,
+           durationOfDelivery: 35,
+           restaurantReviewValue: 4.1,
+         },
+         {
+           _id: "12345-2",
+           photos: [
+            "https://b.zmtcdn.com/data/pictures/chains/2/50382/6fa52a1c7dad34faa24f1bae4e7159de.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+           ],
+           name: "Domino's Pizza",
+           cuisine: ["Pizza", "Fast Food", "Desserts", "Beverages"],
+           averageCost: 150,
+           isPro: false,
+           isOff: false,
+           durationOfDelivery: 30,
+           restaurantReviewValue: 4.0,
+         },
+       ]);
+
   return (
     <>
         <DeliveryCarousal />
         {/* <Brand /> */}
-        <RestaurantCard />
+        <div className='flex justify-between flex-wrap'>
+             {restaurantList.map((restaurant) => (
+              <RestaurantCard {...restaurant} key={restaurant._id} />
+             ))}
+        </div>
     </>
   );
 };
--- client/src/Components/RestaurantCard.jsx
+++ client/src/Components/RestaurantCard.jsx
@@ -5,38 +5,50 @@ import { AiTwotoneStar } from "react-icons/ai";
 
 
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
     <>
     
-       <div className='mb-20 bg-white p-2 w-full md:w-1/2 lg:w-1/3 hover:shadow-lg rounded-xl transition duration-500 ease-in-out'>
+       <div className='mb-4   bg-white p-4 w-full md:w-1/2 lg:w-1/3 hover:shadow-lg rounded-xl transition duration-500 ease-in-out'>
            
            <div className='w-full h-56  lg:h-64 relative '>
 
              <div className='absolute bottom-2 w-full flex items-end justify-between'>
                  <div className='flex flex-col gap-2 items-start'>
-                   <span className='bg-zomato-400 text-white px-2 py-1 rounded text-sm'>Pro extra 10% off</span>
-                   <span className='bg-blue-600 text-white px-2 py-1 rounded text-sm'>₹80 OFF</span>
+                   
+                   {
+                    props.isPro && (
+                      <span className='bg-zomato-400 text-white px-2 py-1 rounded text-sm'>Pro extra 10% off</span>
+                    )
+                   }
+
+                   {
+                    props.isOff && (
+                     <span className='bg-blue-600 text-white px-2 py-1 rounded text-sm'>₹{ `${props.isOff}` } OFF </span>
+                    )
+                   }
+
+                   
                  </div>
-              <span className='bg-white bg-opacity-60 p-1 rounded mr-1'> 42 min</span>
+              <span className='bg-white bg-opacity-60 p-1 rounded mr-1'>{ props.durationOfDelivery } min</span>
              </div>
 
-              <img src='https://b.zmtcdn.com/data/pictures/2/18503752/a1cd82498e16a143ecda57f57cb353b3.jpg?output-format=webp' 
+              <img src={ props.photos.length && props.photos[0]}
               alt='food'
               className='w-full h-full rounded-xl'
               />
            </div>
            < div className='flex flex-col gap-3 my-2'>
                 <div className='flex items-center justify-between '>
-                    <h4 className='text-xl font-medium'>Fish Curry</h4>
+                    <h4 className='text-xl font-medium'> { props.name } </h4>
                     <span className='bg-green-800 text-white text-sm p-1 rounded flex items-center '>
-                        5 <AiTwotoneStar/>
+                        {props.restaurantReviewValue} <AiTwotoneStar/>
                     </span>
                 </div>
               
                 <div className='flex items-center justify-between text-grey-600'>
-                    <p>Fish Curry, South Indian Traditional <br/> style, TamilNadu</p>
-                    <p> ₹ 559 </p>
+                    <p>{ props.cuisine.join(", ")}</p>
+                    <p> ₹ {props.averageCost} for one</p>
                 </div>
                 </div>
        </div>

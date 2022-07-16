272833df7c4790f057508713bd88595214718316
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-16 22:15:23 +0530

    nutrition carousal

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// component
+import NutritionCarousal from './NutritionCarousal';
+
+
+const Nutrition = () => {
+  return (
+    <div>
+        <NutritionCarousal/>
+    </div>
+  )
+}
+
+export default Nutrition ;

14f2d0bb8058fd3a570060069384469314110330
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-17 18:15:10 +0530

    nutrition carousal

--- client/src/Components/Dining/DiningCarousal.jsx
+++ client/src/Components/Dining/DiningCarousal.jsx
@@ -7,15 +7,42 @@ import { NextArrow, PrevArrow } from '../CarousalArrow';
 
 const DiningCarousal = () => {
 
-    const settings = {
-        arrows: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-      };
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className=''>
--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 
 // component
+import NutritionHeroCarousal from './NutritionHeroCarousal';
 import NutritionCarousal from './NutritionCarousal';
+
 
 
 const Nutrition = () => {
   return (
     <div>
+        <NutritionHeroCarousal/>
+
+        <div className='my-6'>
         <NutritionCarousal/>
+        </div>
+
     </div>
   )
 }

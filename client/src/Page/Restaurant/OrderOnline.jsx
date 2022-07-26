c2bca26ab467f5c3b9d1f6cce78e201779798bce
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-27 00:43:13 +0530

    recommended

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 
 // component
 import FloatMenuBtn from '../../Components/restaurant/Order-Onine/FloatMenuBtn';
+import MenuListcontainer from '../../Components/restaurant/Order-Onine/MenuListcontainer';
+
+
 
 const OrderOnline = () => {
   return (
     <>
-     <div className='w-fill'>
-        <FloatMenuBtn/>
-     </div>
+      <div className='w-full'>
+        <aside className='hidden md:flex gap-3 flex flex-col border-r h-screen overflow-y-scroll border-grey-500  w-1/4'>
+          <MenuListcontainer/>
+          <MenuListcontainer/>
+          <MenuListcontainer/>
+          <MenuListcontainer/>
+          <MenuListcontainer/>
+        </aside>
+        <div className='w-full md:w-3/4'></div>
+      </div>
+      <FloatMenuBtn />
     </>
   )
 }
 
-export default OrderOnline ;
+export default OrderOnline;

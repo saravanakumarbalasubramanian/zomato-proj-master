a8723f681faf835d53c74d45d6b932f1443979fd
saravanakumarbalasubramanian <bsaravana2003@gmail.com>
2022-07-27 01:44:56 +0530

    order-online

---
+++ client/src/Components/restaurant/Order-Onine/FoodItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import { BsPlus } from "react-icons/bs"
+
+
+
+
+
+const FoodItem = (props) => {
+    return (
+        <>
+            <div className='flex items-start gap-2  '> 
+                <div className='w-3/12 md:h-28 lg:h-36 h-24 md:px-7 '>
+                    <img 
+                    src={props.image} 
+                    alt="image" 
+                    className='w-full h-full rounded-lg' 
+                    />
+                </div>
+
+                <div className='w-3/4 md:w-7/12 flex flex-col gap-1 '>
+                   
+                    <div className='flex items-center justify-between'>
+                    <h3>{props.title}</h3>
+                    <button className='md:hidden flex items-center gap-1 text-zomato-400 bg-zomato-50 border border-zomato-400 px-2 py-1 rounded-lg '>
+                         Add <BsPlus />
+                    </button>
+                    </div>
+                   
+                    <ReactStars count={5} value={props.rating} />
+                    <h5>{props.price}</h5>
+                    <p className='truncate'>
+                        {props.description}
+                    </p>
+                </div>
+                <div className='hidden md:block w-2/12'>
+                    <button className=' flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-4 py-2 rounded-lg '>
+                        <BsPlus /> Add
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FoodItem;
--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,23 +1,69 @@
 import React from 'react';
+import {GiCompass} from "react-icons/gi"
+import {BsClockHistory} from "react-icons/bs"
 
 // component
 import FloatMenuBtn from '../../Components/restaurant/Order-Onine/FloatMenuBtn';
 import MenuListcontainer from '../../Components/restaurant/Order-Onine/MenuListcontainer';
-
+import FoodItem from '../../Components/restaurant/Order-Onine/FoodItem';
 
 
 const OrderOnline = () => {
   return (
     <>
-      <div className='w-full'>
+      <div className='w-full flex'>
+     
         <aside className='hidden md:flex gap-3 flex flex-col border-r h-screen overflow-y-scroll border-grey-500  w-1/4'>
           <MenuListcontainer/>
           <MenuListcontainer/>
-          <MenuListcontainer/>
-          <MenuListcontainer/>
-          <MenuListcontainer/>
         </aside>
-        <div className='w-full md:w-3/4'></div>
+      
+      
+        <div className='w-full md:w-3/4 mx-5'>
+
+          <h2 className='text-xl font-semibold'> Order Online</h2>
+          <h4 className='flex items-center gap-2 font-light text-grey-200'><GiCompass/>Live track your order | <BsClockHistory/> 55 min </h4>
+       
+        <section>
+          <FoodItem 
+          image="https://b.zmtcdn.com/data/dish_photos/6d2/a13ed20529fda1ba7bd86fc094f426d2.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+          title="Chicken Roll" 
+          rating="5" 
+          price="₹150" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus perferendis explicabo magnam reprehenderit omnis, amet ab magni saepe. Placeat ipsum tempora dolor maiores perspiciatis iusto? Non asperiores illum sapiente."
+          />
+          <FoodItem 
+          image="https://b.zmtcdn.com/data/dish_photos/6d2/a13ed20529fda1ba7bd86fc094f426d2.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+          title="Chicken Roll" 
+          rating="5" 
+          price="₹150" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus perferendis explicabo magnam reprehenderit omnis, amet ab magni saepe. Placeat ipsum tempora dolor maiores perspiciatis iusto? Non asperiores illum sapiente."
+          />
+          <FoodItem 
+          image="https://b.zmtcdn.com/data/dish_photos/6d2/a13ed20529fda1ba7bd86fc094f426d2.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+          title="Chicken Roll" 
+          rating="5" 
+          price="₹150" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus perferendis explicabo magnam reprehenderit omnis, amet ab magni saepe. Placeat ipsum tempora dolor maiores perspiciatis iusto? Non asperiores illum sapiente."
+          />
+          <FoodItem 
+          image="https://b.zmtcdn.com/data/dish_photos/6d2/a13ed20529fda1ba7bd86fc094f426d2.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+          title="Chicken Roll" 
+          rating="5" 
+          price="₹150" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus perferendis explicabo magnam reprehenderit omnis, amet ab magni saepe. Placeat ipsum tempora dolor maiores perspiciatis iusto? Non asperiores illum sapiente."
+          />
+          <FoodItem 
+          image="https://b.zmtcdn.com/data/dish_photos/6d2/a13ed20529fda1ba7bd86fc094f426d2.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*" 
+          title="Chicken Roll" 
+          rating="5" 
+          price="₹150" 
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus perferendis explicabo magnam reprehenderit omnis, amet ab magni saepe. Placeat ipsum tempora dolor maiores perspiciatis iusto? Non asperiores illum sapiente."
+          />
+        </section>
+        </div>
+     
+
       </div>
       <FloatMenuBtn />
     </>
